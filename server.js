80424aa9d44e13bd5e2197e935b3b9c0eaf0a6bb
Rich Capuy <rccapuy@gmail.com>
2020-05-23 21:02:37 -0400

    First prompt and switch cases

--- server.js
+++ server.js
@@ -24,6 +24,49 @@ connection.connect(function (err) {
 	startApp();
 });
 
+// Initial prompt and switch cases depending on selection
 function startApp(){
-	
+	inquirer
+    .prompt({
+        name: "selections",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+                "View All Employees",
+                "View All Department",
+                "View All Roles",
+                "Add An Employee",
+                "Add a Department",
+				"Add a Role",
+				"Update an Employee Role",
+                "EXIT"
+        ]
+    }).then(function (answer) {
+        switch (answer.selections) {
+            case "View All Employees":
+                viewEmployees();
+                break;
+            case "View All Department":
+                viewDepartments();
+                break;
+            case "View All Roles":
+                viewRoles();
+                break;
+            case "Add An Employee":
+                addEmployee();
+                break;
+            case "Add a Department":
+                addDepartment();
+                break;
+            case "Add a Role":
+                addRole();
+				break;
+			case "Update an Employee Role":
+				updateRole();
+				break;
+            case "EXIT": 
+                connection.end();
+                break;
+        }
+    })
 }
